519aa50322a32471b10901c35f10f33943b6beaa
emi <emi.mnemonic@gmail.com>
2016-11-01 19:18:59 +1300

    Include top navigation, hover dropdown and responsive hamburger menu icon

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 $(document).ready(function ()  {
+
+    var items = $('ul.top-nav').children('li:not(.menu-icon)');
+
     $('#open-menu').click(function () {
-        $('#icon').toggleClass('close');
+        $(this).toggleClass('close');
+        items.toggleClass('active');
+    });
+
+    $('li.dropdown').hover(function () {
+        $('.dropdown-content').toggle();
+        $('.main').toggleClass('hover-fade');
     });
 });
