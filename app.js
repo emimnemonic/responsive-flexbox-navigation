3f9a37d39b965585d6bb9931e1da2717306dce7d
emi <emi.mnemonic@gmail.com>
2016-11-02 12:01:53 +1300

    Use pure css to trigger dropdown menu

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ $(document).ready(function ()  {
     });
 
     $('li.dropdown').hover(function () {
-        $('.dropdown-content').toggle();
         $('.main').toggleClass('hover-fade');
     });
 });
